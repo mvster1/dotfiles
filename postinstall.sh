606b532b0331ef8841f45fb5472e02158cc0b56d
mvster1 <146800336+mvster1@users.noreply.github.com>
2023-10-07 10:31:04 -0300

    post-install script

--- postinstall.sh
+++ postinstall.sh
@@ -6,6 +6,8 @@ sudo dnf install rpmfusion-free-release-38.noarch.rpm
 sudo dnf install rpmfusion-nonfree-release-38.noarch.rpm
 sudo dnf update
 
+rm rpmfusion-free-release-38.noarch.rpm fedorapmfusion-nonfree-release-38.noarch.rpm
+
 # installing additional packages
 sudo dnf install Xorg bspwm sxhkd polybar rofi neovim thunar gimp xfce4-terminal xset ffmpeg neofetch
 sudo dnf update

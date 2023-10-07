f5e0dd96668bd69adecb416a98e21a456bc08c6e
mvster1 <146800336+mvster1@users.noreply.github.com>
2023-10-07 10:44:29 -0300

    post-install automation

--- postinstall.sh
+++ postinstall.sh
@@ -1,17 +1,31 @@
 # installing rpmfusion
+echo "getting rpmfusion repo files (free & non-free)..."
 wget https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-38.noarch.rpm
 wget https://mirrors.rpmfusion.org/nonfree/fedorapmfusion-nonfree-release-38.noarch.rpm
 
+echo "installing rpmfusion repo files..."
 sudo dnf install rpmfusion-free-release-38.noarch.rpm
 sudo dnf install rpmfusion-nonfree-release-38.noarch.rpm
-sudo dnf update
 
 rm rpmfusion-free-release-38.noarch.rpm fedorapmfusion-nonfree-release-38.noarch.rpm
 
-# installing additional packages
-sudo dnf install Xorg bspwm sxhkd polybar rofi neovim thunar gimp xfce4-terminal xset ffmpeg neofetch
+echo "updating..."
 sudo dnf update
 
+# installing additional packages
+echo "installing packages..."
+sudo dnf install Xorg bspwm sxhkd polybar rofi neovim thunar gimp xfce4-terminal xset ffmpeg neofetch
+echo "updading..."
+sudo dnf update
+
+# adding Martian Mono fonts
+git clone https://github.com/evilmartians/mono
+mkdir -p ~/.fonts/MartianMono
+mv mono ~/.fonts/MartianMono/
+
+# cleaning up
+echo "cleaning things up..."
 sudo dnf autoremove
 sudo dnf clean all
 sudo dnf clean packages
+sudo dnf update

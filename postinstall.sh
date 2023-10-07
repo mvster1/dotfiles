1db4f4b91a3bae05a4ebb37a5e25e8c6c341852a
mvster1 <146800336+mvster1@users.noreply.github.com>
2023-10-07 10:29:52 -0300

    post-install script

--- postinstall.sh
+++ postinstall.sh
@@ -7,5 +7,9 @@ sudo dnf install rpmfusion-nonfree-release-38.noarch.rpm
 sudo dnf update
 
 # installing additional packages
-sudo dnf install Xorg bspwm sxhkd polybar rofi neovim thunar xset ffmpeg git neofetch
+sudo dnf install Xorg bspwm sxhkd polybar rofi neovim thunar gimp xfce4-terminal xset ffmpeg neofetch
 sudo dnf update
+
+sudo dnf autoremove
+sudo dnf clean all
+sudo dnf clean packages

a0a7c22f42621ce2ca41bb780819d67d3137409a
mvster1 <146800336+mvster1@users.noreply.github.com>
2024-01-15 23:34:34 -0300

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -14,7 +14,7 @@ sudo dnf update
 
 # installing additional packages
 echo "installing packages..."
-sudo dnf install Xorg xinit xset xsetroot bspwm sxhkd polybar arc-theme feh rofi neovim thunar gimp firefox kitty ffmpeg nodejs pulseaudio pavucontrol neofetch
+sudo dnf install Xorg xinit xset xsetroot bspwm sxhkd polybar arc-theme feh rofi neovim thunar gimp firefox kitty ffmpeg nodejs pulseaudio pavucontrol neofetch libXinerama cups-libs dbus-glib
 
 # installing additional fonts
 sudo dnf group install fonts

c04c4a495c83d0dd3896a242809224532bc77359
mvster1 <146800336+mvster1@users.noreply.github.com>
2024-11-21 06:02:45 -0300

    Create wacom_settings.sh

---
+++ wacom_settings.sh
@@ -0,0 +1,5 @@
+xsetwacom --set "Wacom One by Wacom S Pen stylus" Rotate half
+# xsetwacom --set "Wacom One by Wacom S Pen stylus" Area 0 0 30000 30000
+xsetwacom --set "Wacom One by Wacom S Pen stylus" Area 0 0 40000 40000
+xsetwacom --set "Wacom One by Wacom S Pen stylus" Mode relative
+xsetwacom --set "Wacom One by Wacom S Pen stylus" CursorProximity 15
